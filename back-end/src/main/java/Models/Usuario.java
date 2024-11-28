21d3892eaf8ccedf723c4701b510dfb3431ac004
Gabriel Correia <85449757+Gabriel-Correia-Silva@users.noreply.github.com>
2024-11-28 10:25:31 -0300

    model de usuario e enum de role

---
+++ back-end/src/main/java/Models/Usuario.java
@@ -0,0 +1,16 @@
+package Models;
+
+import javax.swing.*;
+
+public class Usuario {
+
+    private long idUsuario;
+
+    private String Nome;
+
+    private String Email;
+
+    private String Senha;
+
+    private Role role;
+}
